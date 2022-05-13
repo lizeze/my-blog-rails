38652603006271943d980ed3ceefa343a16e573f
zeze.li <lizeze.boy@gmail.com>
2022-05-13 16:18:50 +0800

    渲染markdown

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,10 +1,12 @@
+require 'redcarpet'
+require 'redcarpet/render_strip'
 class IndexController < ApplicationController
 
     def index
-        render :template =>'index' 
-
+  markdown = Redcarpet::Markdown.new(Redcarpet::Render::StripDown)
+       @html= Markdown.new("**This** _is_ an [example](http://example.org/).").to_html
+       render :template =>'index' 
     end
 
   
-
 end

4f0f876bfeedfb9815f2f866e57d4251f6ddbbae
zeze.li <lizeze.boy@gmail.com>
2022-05-16 16:24:52 +0800

    article  demo

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,8 +1,9 @@
 class AboutController < ApplicationController
 
     def index
-
         render :template=>'about/index'
-        
+    end
+    def show
+        @article = Article.find(params[:id])
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "index#index"
+  root "articles#index"
   resources :tag
   resources :post
   get '/archive',action: :index,controller: 'archive'
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   get '/post',action: :index,controller:'post'
   get '/add/post', action: :post_add,controller:'post'
   get '/all/post',action: :show ,controller:'post'
- 
 
+  resources :articles
   
 end

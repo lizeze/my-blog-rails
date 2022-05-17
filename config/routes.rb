2c2ed8acb816fb4af74c25c471feec362247137a
zeze.li <lizeze.boy@gmail.com>
2022-05-17 11:02:01 +0800

    文件上传

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  get 'resumes/index'
+  get 'resumes/new'
+  get 'resumes/create'
+  get 'resumes/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :resumes, only: [:index, :new, :create, :destroy]
+  root "resumes#index"
   # Defines the root path route ("/")
-  root "articles#index"
+  # root "articles#index"
   resources :tag
   get '/archive',action: :index,controller: 'archive'
   get '/about',action: :index ,controller:'about'
